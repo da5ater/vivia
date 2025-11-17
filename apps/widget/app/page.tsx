e3113c30f4c27ca129f9d8e5474c8113d5904147
da5ater <vincentvan5ater@gmail.com>
2025-11-17 15:26:39 +0200

    feat: enhance user management with add user functionality and update project dependencies

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import { Button } from "@workspace/ui/components/button";
+import { add } from "@workspace/math/add";
+import { Input } from "@workspace/ui/components/input";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.createUser);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app/web</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div>
+      {users === undefined && <p>Loading...</p>}
+      {users && (
+        <>
+          <ul>
+            {users.map((user) => (
+              <li key={user._id}>{user.name}</li>
+            ))}
+          </ul>
+          <Button
+            onClick={() =>
+              addUser({ name: "Alice", email: "alice@example.com" })
+            }
+          >
+            Add User
+          </Button>
+        </>
+      )}
     </div>
   );
 }
